91ea47aac9ffa08cb94789fab8d9e2d29ecca839
feenko <147188774+feenko@users.noreply.github.com>
2024-07-21 16:46:25 +0200

    feat: discord embed

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -5,6 +5,13 @@ import { GeistSans } from "geist/font/sans";
 
 export const metadata: Metadata = {
     title: "feenko.lol",
+    description: "Software Engineer, Designer, and Music Enthusiast",
+    other: {
+        "og:description": "Software Engineer, Designer, and Music Enthusiast",
+        "og:url": "https://feenko.lol/",
+        "og:title": "feenko.lol",
+        "theme-color": "#010101",
+    },
 };
 
 export default function RootLayout({
