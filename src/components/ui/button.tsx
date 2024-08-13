7186ee46d20d3675f5448349885459ccb1d34ada
Szymon <147188774+feenko@users.noreply.github.com>
2024-08-13 03:26:34 +0200

    feat: button component

---
+++ src/components/ui/button.tsx
@@ -0,0 +1,49 @@
+import { cn } from "@/lib/utils";
+
+type ButtonVariant = "primary" | "secondary";
+type ButtonSize = "sm" | "md" | "lg";
+
+interface ButtonProps extends React.ButtonHTMLAttributes<HTMLButtonElement> {
+    variant?: ButtonVariant;
+    size?: ButtonSize;
+}
+
+const Button: React.FC<ButtonProps> = ({
+    children,
+    variant,
+    size,
+    ...props
+}) => {
+    variant = variant || "primary";
+    size = size || "md";
+
+    const baseStyle = "rounded-md focus:outline-none transition duration-200";
+
+    const variantStyle: Record<ButtonVariant, string> = {
+        primary: "bg-white text-black hover:bg-neutral-300",
+        secondary:
+            "bg-black text-white border border-neutral-800 hover:bg-neutral-950",
+    };
+
+    const sizeStyle: Record<ButtonSize, string> = {
+        sm: "px-2 py-1.5 text-sm",
+        md: "px-3 py-1.5 text-base",
+        lg: "px-4 py-2 text-lg",
+    };
+
+    return (
+        <button
+            {...props}
+            className={cn(
+                baseStyle,
+                variantStyle[variant],
+                sizeStyle[size],
+                props.className
+            )}
+        >
+            {children}
+        </button>
+    );
+};
+
+export default Button;
