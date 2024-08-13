ec1263f1fd7fcc3c544bd66230d73ee2386ef470
Szymon <147188774+feenko@users.noreply.github.com>
2024-08-13 03:29:41 +0200

    feat: experience page

---
+++ src/app/experience/page.tsx
@@ -0,0 +1,9 @@
+import PageReserved from "@/components/layout/page-reserved";
+import { Metadata } from "next";
+
+export const metadata: Metadata = {
+    title: "experience",
+    description: "My experience in software engineering and design",
+};
+
+export default PageReserved;
